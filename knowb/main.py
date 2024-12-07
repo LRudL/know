9bf9336b8a4b893b14e4e3b6c60b02ba0db78d66
LRudL <rudolf.laine@gmail.com>
2024-12-07 21:07:33 +0000

    Adding new main including TSS calls

--- knowb/main.py
+++ knowb/main.py
@@ -6,6 +6,7 @@ from src.services.security import security
 from src.api.routes import documents
 from src.api.routes import chat
 from src.api.routes import test
+from src.api.routes import tts_routes
 
 app = FastAPI()
 
@@ -15,6 +16,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    # allow_websockets=True
 )
 
 # Include routers
@@ -23,3 +25,4 @@ app.include_router(content_map.router, prefix="/api/content_map")
 app.include_router(chat.router, prefix="/api/chat")
 app.include_router(test.router, prefix="/api/test")
 app.include_router(learning.router, prefix="/api/learning")
+app.include_router(tts_routes.router)
